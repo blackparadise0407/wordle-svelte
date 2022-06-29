46cc36ce091149c5827649aa400f36ef4bd1eb6e
blackparadise0407 <blackparadise0407@gmail.com>
2022-06-29 17:06:52 +0700

    Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   import Board from "./lib/Board.svelte";
   import ToastProvider from "./lib/ToastProvider.svelte";
   import Score from "./lib/Score.svelte";
+  import Footer from "./lib/Footer.svelte";
 
   import "./index.css";
 
@@ -22,14 +23,21 @@
 </script>
 
 <main>
+  <h1>Worlde Svelte</h1>
   <Score />
   {#if !!word}
     <Board target={word} onNextWord={handleRandomNextWord} />
   {/if}
   <ToastProvider />
+  <Footer />
 </main>
 
 <style>
+  h1 {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+
   main {
     display: grid;
     place-content: center;
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>
+  <p>Made with Svelte. From Kyle with ❤️</p>
+</footer>
+
+<style>
+  footer p {
+    position: fixed;
+    width: 100vw;
+    left: 0;
+    bottom: 0;
+    text-align: center;
+  }
+</style>
